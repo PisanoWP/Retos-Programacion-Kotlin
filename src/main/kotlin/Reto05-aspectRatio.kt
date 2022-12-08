c8e9d94217d295873ca77c643391a182a3a34ae9
BNNDES01C\jc.gomezlobo <jc.gomezlobo@bannia.es>
2022-12-08 11:26:21 +0100

    Reto #6 INVIRTIENDO CADENAS

---
+++ src/main/kotlin/Reto05-aspectRatio.kt
@@ -0,0 +1,52 @@
+import java.text.DecimalFormat
+
+fun main(){
+
+ /*
+ * Reto #4
+ * ÁREA DE UN POLÍGONO
+ *
+ * Enunciado: Crea UNA ÚNICA FUNCIÓN (importante que sólo sea una) que sea capaz de calcular y retornar el área de un polígono.
+ * - La función recibirá por parámetro sólo UN polígono a la vez.
+ * - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
+ * - Imprime el cálculo del área de un polígono de cada tipo.
+ *
+ *  https://github.com/mouredev/Weekly-Challenge-2022-Kotlin
+ *
+ */
+
+    val triangulo = Triangulo(2.0, 3.5)
+    val rectangulo = Rectangulo(3.5, 6.0)
+    val cuadrado = Cuadrado(2.6)
+
+    areaPoligono(triangulo)
+    areaPoligono(rectangulo)
+    areaPoligono(cuadrado)
+
+}
+
+fun areaPoligono(poligono: Poligono){
+    val df = DecimalFormat("#.##")
+    val roundoff = df.format(poligono.getArea())
+    println(roundoff)
+}
+
+interface Poligono{
+    fun getArea():Double
+}
+
+class Triangulo(var base:Double, var altura:Double):Poligono{
+    override fun getArea(): Double {
+        return (base * altura) / 2
+    }
+}
+class Rectangulo(var base:Double, var altura:Double):Poligono{
+    override fun getArea(): Double {
+        return (base * altura)
+    }
+}
+class Cuadrado(var lado:Double):Poligono{
+    override fun getArea(): Double {
+        return lado * lado
+    }
+}
